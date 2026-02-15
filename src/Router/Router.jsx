233c16cc4ec24dd994a6464f30be5c6ef3de314e
fayoz <fayoz@fayoz.fayoz>
2026-02-15 18:46:18 +0500

    Register Section Done

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Entry from "../pages/Entry/Entry";
 import SellerHome from "../pages/seller/SellHome/SellerHome";
 import Home from "../pages/buyer/Home/Home";
+import Register from "../pages/seller/Register/Register";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Entry />} />
         <Route path="/seller" element={<SellerHome />} />
         <Route path="/buyer" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
